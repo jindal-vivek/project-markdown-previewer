221bb4fa907d971fb888af796186dadff2e0c556
Vivek Jindal <jindal.vivek@hotmail.com>
2019-08-24 23:19:30 +0530

    Updates

---
+++ precache-manifest.2b9628d59bc11da6d2e5b5818c750e1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "221ea6529dfb52717030168bc104bd12",
+    "url": "/project-markdown-previewer/index.html"
+  },
+  {
+    "revision": "1b6884c73c9f8b25e56d",
+    "url": "/project-markdown-previewer/static/css/main.5f6544a5.chunk.css"
+  },
+  {
+    "revision": "9968e11228fa3dc12038",
+    "url": "/project-markdown-previewer/static/js/2.962718ac.chunk.js"
+  },
+  {
+    "revision": "1b6884c73c9f8b25e56d",
+    "url": "/project-markdown-previewer/static/js/main.fef7dc83.chunk.js"
+  },
+  {
+    "revision": "6245c954fd0e7b7ba3fd",
+    "url": "/project-markdown-previewer/static/js/runtime~main.0596806e.js"
+  }
+]);
